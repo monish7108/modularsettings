718d6acebba029b9a34e1cd36e1636142cacfb9e
Sam Simmons <sam@samiconductor.com>
2012-08-04 21:24:42 -0500

    fix database password typo

--- modularsettings/settings/databases.py
+++ modularsettings/settings/databases.py
@@ -20,7 +20,7 @@ def require(DEV_ENV, BASE_PATH, DATABASE_USER, DATABASE_PASSWORD,
             'default': {
                 'ENGINE': 'django.db.backends.postgresql_psycopg2',
                 'USER': DATABASE_USER,
-                'PASSWORD': DATABASE_USER,
+                'PASSWORD': DATABASE_PASSWORD,
                 'HOST': DATABASE_HOST,
                 'PORT': DATABASE_PORT,
             }
