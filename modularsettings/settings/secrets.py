94ac9c2f581043f89620e29e484bfd31602a39d4
Sam Simmons <sam@samiconductor.com>
2012-08-06 06:23:33 -0500

    improved secrets example

--- modularsettings/settings/secrets.py
+++ modularsettings/settings/secrets.py
@@ -1,7 +1,15 @@
+import env
+
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '000000000000000000000000000000000000000000000000000000'
 
-DATABASE_USER = 'secret user'
-DATABASE_PASSWORD = 'secret password'
-DATABASE_HOST = 'secret host'
-DATABASE_PORT = '12345'
+if env.TEST_ENV:
+    DATABASE_USER = 'test user'
+    DATABASE_PASSWORD = 'test password'
+elif env.PROD_ENV:
+    DATABASE_USER = 'prod user'
+    DATABASE_PASSWORD = 'prod password'
+
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = '5432'
