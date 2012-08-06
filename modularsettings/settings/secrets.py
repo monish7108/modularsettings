82b9dcff670af7da886346fab5d8d4314b0836db
Sam Simmons <sam@samiconductor.com>
2012-08-06 00:02:27 -0500

    included secrets for example

---
+++ modularsettings/settings/secrets.py
@@ -0,0 +1,7 @@
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '000000000000000000000000000000000000000000000000000000'
+
+DATABASE_USER = 'secret user'
+DATABASE_PASSWORD = 'secret password'
+DATABASE_HOST = 'secret host'
+DATABASE_PORT = '12345'
